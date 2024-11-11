310ce72b33931097d9a1427cea06c9be1221727b
schenok <m.gladyshew@yandex.ru>
2024-11-11 06:35:04 +0300

    feat: move the db connection to the database module

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LinkModule } from '../link/link.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [
+    DatabaseModule,
     LinkModule,
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT as unknown as number,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: true, // for development
     }),
   ],
   controllers: [],
